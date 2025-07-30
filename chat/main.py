e5cd1f1655039230186668213fcbf4780bde603f
wuyuanwuhui99 <275018723@qq.com>
2025-07-30 21:19:03 +0800

    修改文件名称

--- chat/main.py
+++ chat/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from chat.routers.routes import router as chat_router
-from chat.config.config import settings
+from chat.routers import router as chat_router
+from chat.config import settings
 
 app = FastAPI(title="AI Chat Service")
 app.include_router(chat_router)
